c9203e9c05d77d3e665e0eb3d779ee1b6ba58d04
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-05-14 10:44:43 +0700

    use attribute decision to store the next action

--- BuildPolygonState.cpp
+++ BuildPolygonState.cpp
@@ -14,12 +14,30 @@ class InitState{
     public:
         int r;
         int c;
+        char decision = 'S';
         InitState(int r, int c){
             this->r = r;
             this->c = c;
         }
-        virtual char nextState(int **array, int R, int C);
-        virtual void move();
+        virtual void nextState(int **array, int ROW, int COL){
+            if(c < COL - 1){
+                if(array[r][c + 1] == 1){
+                    decision = 'R';
+                    return; //the direction is RIGHT
+                }
+                else{
+                    decision = 'S';
+                    return;//Still find the first non-empty cell
+                }
+            }
+            else{//reach the boundary
+                decision = 'C';
+                return;//change row
+            }
+        }
+        virtual void move(){
+
+        }
     
 };
 
@@ -31,28 +49,33 @@ class RightForwardingState : public virtual InitState
         RightForwardingState(int r, int c) : InitState{r, c}{
 
         }
-        char nextState(int **array, int ROW, int COL){
+        void nextState(int **array, int ROW, int COL){
             if(c < COL - 1){
                 if(array[r][c + 1] == 1){
-                    return 'S';//Still
+                    decision = 'S';
+                    return;//Still
                 }
                 else{
-                    return 'D';//Down
+                    decision = 'D';//Down
+                    return;
                 }
             }
             else{//reach right boundary
                 if(r < ROW - 1){
                     if(array[r + 1][c] == 1){
-                        return 'D';//Down
+                        decision = 'D';//Down
+                        return;
                     }
                     else{
-                        return 'L'; //Quay lui lại
+                        decision = 'L';//Left
+                        return;//Quay lui lại
                         //tức turn left
                         //Ô trống ở dưới hàng này
                     }
                 }
                 else{//đang duyệt hàng cuối cùng
-                    return 'L';//quay lui lại
+                    decision = 'L';//Left
+                    return;//Quay lui lại
                 }
             }
         }
@@ -69,28 +92,33 @@ class LeftForwardingState : public virtual InitState
         LeftForwardingState(int r, int c) : InitState{r, c}{
 
         }
-        char nextState(int **array, int ROW, int COL){
+        void nextState(int **array, int ROW, int COL){
             if(c > 0){
                 if(array[r][c - 1] == 1){
-                    return 'S';//Still
+                    decision = 'S';
+                    return;//Still
                 }
                 else{
-                    return 'D';//Down
+                    decision = 'D';//Down
+                    return;
                 }
             }
             else{//reach right boundary
                 if(r < ROW - 1){
                     if(array[r + 1][c] == 1){
-                        return 'D';//Down
+                        decision = 'D';//Down
+                        return;
                     }
                     else{
-                        return 'L'; //Quay lui lại
+                        decision = 'L';//Left
+                        return;//Quay lui lại
                         //tức turn left
                         //Ô trống ở dưới hàng này
                     }
                 }
                 else{//đang duyệt hàng cuối cùng
-                    return 'L';//quay lui lại
+                    decision = 'L';//Left
+                    return;//Quay lui lại
                 }
             }
         }
