0d74a80def0a2559fa07f0a5f7595a62c682f262
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-05-24 21:48:36 +0700

    a new attribute and the countSharedVertices has side effect

--- RefinePolygons.cpp
+++ RefinePolygons.cpp
@@ -20,15 +20,70 @@ using namespace std;
 
 class RefinePolygons : public BuildingPolygons{
     public:
+        bool sharedVertices[4] = {false, false, false, false};
         RefinePolygons(int Row, int Column, int cordX, int cordY, int width, int length)
             :  BuildingPolygons(Row, Column, cordX, cordY, width, length)
         {
 
         }
 
-
-        void removeEdgesAndVertices(){
+        int countSharedVertices(int indexOfStack, int row, int column, int *first, int *last, int *stack){
+            this->getFourVertecies(indexOfStack, row, column);
+            bool found = false;
+            //for(int i = 0; i < this->NUMBER_STACKS; i++)
+            int count = 0;
             
+            int prev = 0;
+            
+            for(int i = 0; i < this->polygons.size(); i++){
+                for(int j = 0; j < this->polygons.at(i).size(); j++){
+                    if(!sharedVertices[0] && this->polygons.at(i).at(j).p == pA)
+                    {
+                        sharedVertices[0] = true;
+                        count++;
+                        
+                    }
+                    else if(!sharedVertices[1] && this->polygons.at(i).at(j).p == pB)
+                    {
+                        sharedVertices[1] = true; 
+                        count++;
+                    }
+                    else if(!sharedVertices[2] && this->polygons.at(i).at(j).p == pC)
+                    {
+                        sharedVertices[2] = true;
+                        count++;
+                    }
+                    else if(!sharedVertices[3] && this->polygons.at(i).at(j).p == pD)
+                    {
+                        sharedVertices[3] = true;
+                        count++;
+                    }
+                    if(prev != count){
+                        if(*first == -1)
+                            *first = j;
+                        else
+                            *last = j;
+                        prev = count;
+                    }
+                    if(count == 4)
+                        break;
+                }
+                if(count > 0){
+                    *stack = i;
+                    found = true;
+                    break;
+                }
+            }
+            
+            return count;
+        }
+
+        void removeEdgesAndVertices(int indexOfStack, int row, int column){
+            int first = -1, last = -1, stack = -1;
+            int nmrSameVertices = this->countSharedVertices(indexOfStack, row, column, &first, &last, &stack);
+            if(nmrSameVertices == 4){
+
+            }
         }
 };
 #endif
