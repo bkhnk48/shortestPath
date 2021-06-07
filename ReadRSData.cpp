373e8be7391fdc0e4e430d568025c0048c096313
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-06-07 09:58:43 +0700

    parse input file and prepare to generate vector of points

--- ReadRSData.cpp
+++ ReadRSData.cpp
@@ -10,6 +10,8 @@
 #include <queue>
 #include <bits/stdc++.h>
 #include <algorithm>
+#include <fstream>
+
 
 #include "generatePy.cpp"
 
@@ -18,4 +20,37 @@
 
 using namespace std;
 
+vector<point> readRSFile(string fileName){
+
+    vector<point> discreteTrajectory;
+     
+    ifstream infile(fileName);
+    string line;
+    double x, y, distance;
+    string typeOfTraj;
+    getline(infile, line);
+    istringstream firstIss(line);
+    if (firstIss >> x >> y >> typeOfTraj) 
+    { 
+        cout<<"X = "<<x<<" Y = "<<y<<" "<<typeOfTraj<<endl;
+        point p0(x, y);
+        discreteTrajectory.push_back(p0);
+
+        while (getline(infile, line))
+        {
+            istringstream iss(line);
+            
+            if (!(iss >> x >> y >> distance >> typeOfTraj)) 
+            { 
+                break; 
+            } // error
+            cout<<"X = "<<x<<" Y = "<<y<<" dis = "<<distance<<" "<<typeOfTraj<<endl;
+            // process pair (a,b)
+        }
+    }
+    cout<<"Close file"<<endl;
+    infile.close();
+
+    return discreteTrajectory;
+}
 #endif
