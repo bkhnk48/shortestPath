65f401a59a2e6d61fd53eff2e29d36250d2167d1
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-06-08 01:02:12 +0700

    constant static variable

---
+++ Constant.cpp
@@ -0,0 +1,31 @@
+#include <iostream> //cin cout
+#include <vector> //vector
+#include <cstdio> //scanf
+#include <sstream> //ostringstream
+#include <cmath> //pow sqrt
+#include <queue> //priority_queue
+#include <stdlib.h> //atoi
+#include <tuple> //get<n> make_tuple
+#include <chrono>
+#include <queue>
+#include <bits/stdc++.h>
+#include <algorithm>
+#include <fstream>
+
+
+#ifndef _CONSTANT_
+#define _CONSTANT_
+
+using namespace std;
+
+const string LEFT("Left");
+
+const string RIGHT("Right");
+
+const char CIRCLE = 'C';
+
+const double MIN_VELOCITY = 2; //(m/s)
+
+const double MAX_VELOCITY = 2.5; //(m/s)
+
+#endif
