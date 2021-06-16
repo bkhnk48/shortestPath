49f2b6881d6e11d1269adf8574517c9285fba8d6
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-06-17 00:41:05 +0700

    remove redundant code and param

--- generatePy.cpp
+++ generatePy.cpp
@@ -51,47 +51,8 @@ void writeRoute(string file_name, vector<point> &route, float firstAngle, float 
 	ofs.close();
 }
 
-void writeFile(string file_name, vector< vector< lineSegment> > &polygons){
-    if(polygons.size() == 0)
-        return;
-    
-    string str1 = to_string(polygons.size());
-    str1.append("\n");
-    //str1.append("\t");
 
-    for(int i = 0; i < polygons.size(); i++){
-        int numEdges = polygons.at(i).size();
-        str1.append(to_string(numEdges));
-        str1.append("\n");
-        for(int j = 0; j < polygons.at(i).size(); j++){
-            str1.append(to_string(polygons.at(i).at(j).p.x));
-            str1.append(" ");
-            str1.append(to_string(polygons.at(i).at(j).p.y));
-            str1.append(" ");
-            str1.append(to_string(polygons.at(i).at(j).q.x));
-            str1.append(" ");
-            str1.append(to_string(polygons.at(i).at(j).q.y));
-            str1.append("\n");
-        }
-    }
-
-    std::ofstream ofs(file_name.c_str());
-	if (!ofs.good())
-		cout<<"Could not write data to "<<file_name;
-
-	ofs << str1;
-	ofs.close();
-
-    
-}
-
-void runPythonCode(string file_name, vector<point> &route, int* directionAtTheEnd, vector< vector< lineSegment> > &polygons){
-
-    //writeFile("test/polygons.txt", polygons);
-    
-    float scale = 1;
-    float deltaX = 0; //22;
-    float deltaY = 0; //18;
+void runPythonCode(string file_name, vector<point> &route, int* directionAtTheEnd){
 
 	char cmd[50];
     float lastAngle = 0;
