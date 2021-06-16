cb9b46dbcb906f99cbdfa4d58949dde432cc8c0b
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-06-17 06:37:03 +0700

    built-in maps for just loading

---
+++ BuiltInMap.cpp
@@ -0,0 +1,39 @@
+#include <iostream> //cin cout
+#include <vector> //vector
+#include <cstdio> //scanf
+#include <sstream> //ostringstream
+#include <cmath> //pow sqrt
+#include <queue> //priority_queue
+#include <stdlib.h> //atoi
+#include <tuple> //get<n> make_tuple
+#include <chrono>
+//#include "BuildPolygonState.cpp"
+
+#include <cassert>  
+#include <stdio.h>
+
+//So we don't need to write std:: everywhere
+using namespace std;
+
+void twoSeperatePolygons(int** slots){
+    int ROWS = 6;
+    int COLS = 21;
+
+    printf("Two independent polygons in both sides of stack[0]:\n");
+
+    int arr[6][21] 
+    = {
+        1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0,
+    };
+
+    for(int i = 0; i < ROWS; i++){
+        for(int j = 0; j < COLS; j++){
+            slots[i][j] = arr[i][j];
+        }
+    }
+}
