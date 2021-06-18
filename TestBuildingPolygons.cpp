7de6b9b314c4703cd1d95d30cc3ff15b0202bc1d
bkhnk48 <nguyenthanh@soict.hust.edu.vn>
2021-06-18 11:55:41 +0700

    load all four hences

--- TestBuildingPolygons.cpp
+++ TestBuildingPolygons.cpp
@@ -101,7 +101,7 @@ void testFullAVsInStacks(){
 		case 'y':
 		case '1':
 			walls = loadWall(rowsInStack, columnsInStack, 5, widthOfAV, lengthOfAV);
-			generator->loadWall(walls, 3);
+			generator->loadWall(walls);
 			break;
 	
 		default:
